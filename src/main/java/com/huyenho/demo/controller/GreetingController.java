c75dd269baf10bb188dca5e41ec66cce1391b9d1
Huyen Ho <huyenhothi6351@gmail.com>
2024-12-12 08:25:05 +0700

    Done Bai_1

---
+++ src/main/java/com/huyenho/demo/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.huyenho.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(defaultValue = "") String name) {
+        return "Hello " + name + "!!!";
+    }
+}

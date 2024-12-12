e45b3e6ea85f0a84e661a449e786600fb78f86ac
Huyen Ho <huyenhothi6351@gmail.com>
2024-12-12 22:09:03 +0700

    Done

--- src/main/java/com/huyenho/demo/controller/GreetingController.java
+++ src/main/java/com/huyenho/demo/controller/GreetingController.java
@@ -1,10 +1,12 @@
 package com.huyenho.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class GreetingController {
 
     @GetMapping("/greeting")
